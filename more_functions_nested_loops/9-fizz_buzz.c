1c23bd70ce092b69307467540b0e9ff84b879614
Ealise <10503@holbertonstudents.com>
2025-02-24 07:31:34 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,22 +16,26 @@ int main(void)
 {
 	int a = 1;
 
-	while (a < 100)
+	while (a <= 100)
 	{
 		if (((a % 3) == 0) && ((a % 5) == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		if ((a % 3) == 0)
+		else if ((a % 3) == 0)
 		{
 			printf("Fizz ");
 		}
-		if ((a % 5) == 0)
+		else if ((a % 5) == 0)
 		{
 			printf("Buzz ");
 		}
-		printf("%d ", a);
+		else
+		{	
+			printf("%d ", a);
+		}
 		a++;
 	}
+	printf("\n");
 	return (0);
 }
