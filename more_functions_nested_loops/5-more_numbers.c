609581348e057803d739eabd107a9f7b0a40e23f
Ealise <10503@holbertonstudents.com>
2025-02-24 02:25:48 +0000

    print more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14.
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		int b;
+
+		for (b = 0; b <= 14; b++)
+		{
+			if (b <= 9)
+			{
+				_putchar(b + '0');
+			}
+			if (b >= 10)
+			{
+				_putchar((b / 10) + '0');
+				_putchar((b % 10) + '0');
+			}
+		}
+
+
+		_putchar('\n');
+	}
+}
