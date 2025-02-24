76d97f9f612f8a7b6a0680ab67fe706e55f8458a
Ealise <10503@holbertonstudents.com>
2025-02-24 09:21:08 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -21,18 +21,25 @@ int main(void)
 		if (((a % 3) == 0) && ((a % 5) == 0))
 		{
 			printf("FizzBuzz");
+			printf(" ");
 		}
 		else if ((a % 3) == 0)
 		{
 			printf("Fizz");
+			printf(" ");
 		}
 		else if ((a % 5) == 0)
 		{
 			printf("Buzz");
+			printf(" ");
 		}
 		else
 		{
 			printf("%d ", a);
+		}
+		if (a != 100)
+		{
+			printf(" ");
 		}
 
 		a++;
