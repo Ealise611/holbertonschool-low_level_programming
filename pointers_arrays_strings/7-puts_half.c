73bf82954df3d4dd61de17b857df261cafb96b56
Ealise <10503@holbertonstudents.com>
2025-02-26 10:03:43 +0000

    puts half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -15,17 +15,23 @@
 void puts_half(char *str)
 {
 	int len = 0;
-	int n;
+	int start;
 
 	while (str[len] != '\0')
 	{
 		len++;	
 	}
-	n = ((len + 1) / 2);
-
-	while (str[len] != '\0')
+	if (len % 2 == 0)
 	{
-		_putchar(str[len]);
+		start = len / 2;
+	}
+	else
+		start = (len + 1) / 2;
+
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
 	}
 	_putchar('\n');
 }
