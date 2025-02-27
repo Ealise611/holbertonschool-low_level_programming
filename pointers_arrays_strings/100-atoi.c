d841af19c66185a28796fbf43f1dc02280442225
Ealise <10503@holbertonstudents.com>
2025-02-27 10:05:44 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,20 +2,38 @@
 
 int _atoi(char *s)
 {
-	int count = 0;
+	int count_of_sign = 0;
+	int result = 0;
+	int sign = 1;
 
-	while (*s != '\0')
+	while ((*s < '0' || *s > '9') && *s != '\0')/*this will iterate through the loop until we find everything up to the first int*/
 	{
-		while ( *s >=  48 && *s <= 57)
+		if ( *s == '-')/*this will find all the '-' signs and add to it*/
 		{
-			_putchar(*s);
-			*s++;
-			
+			count_of_sign++;
 		}
 		
-		*s++;
+		s++;
 
-		_putchar('\n');
 	}
+	if (count_of_sign % 2 == 1)
+	{
+		sign = -1;
+	}
+	while ((*s >= '0' && *s <= '9') && *s != '\0')/*finding the integers after we did the first checking*/
+	{
+		if (!result)/*checking if there is a result*/
+		{
+			result = (*s - '0') * sign;
+		}
+		else
+		{
+			result = ((result * 10)  + ((*s - '0') * sign));/*find the first number and times 10 to make it a double digit, and add the second single digit that we find*/
+		}
+		s++;
+	}
+	return (result);
+
+
 }
 		
