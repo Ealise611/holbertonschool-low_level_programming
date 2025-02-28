5c5b54aad40f9b4acfff1c7b6623cef932932b69
Ealise <10503@holbertonstudents.com>
2025-02-28 09:55:36 +0000

    capitalise

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes the first letter of each word in a string.
+ * @s: The input string.
+ *
+ * Return: Pointer to the modified string.
+ */
+
+char *cap_string(char *s)
+{
+	int count = 0;
+	int capitalise = 1;/*check if needs capitalise*/
+
+	while (s[count] != '\0')
+	{
+		if (s[count] == ' ' || s[count] == '	' || s[count] == '\n'
+		|| s[count] == ',' || s[count] == '!' || s[count] == '?'
+		|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+		|| s[count] == '{' || s[count] == '}')
+		{
+			capitalise = 1;
+		}
+		else
+		{
+			if (capitalise == 1 && s[count] >= 'a' && s[count] <= 'z')
+			{
+				s[count] = s[count] - 32;
+				capitalise = 0;
+			}
+			else
+			{
+				capitalise = 0;
+			}
+		}
+		count++;
+	}
+	return (s);
+}
