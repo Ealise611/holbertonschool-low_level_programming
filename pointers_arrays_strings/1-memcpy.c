03a726c0a2f7fbae548d1ddfa54e5dbc174e4515
Ealise <10503@holbertonstudents.com>
2025-03-03 01:17:59 +0000

    cp num of n from src to dest

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory area.
+ * @dest: Pointer to the destination memory area.
+ * @src: Pointer to the source memory area.
+ * @n: Number of bytes to copy.
+ *
+ * This function copies @n bytes from the memory area @src to the memory
+ * area @dest. It returns a pointer to the destination memory area @dest.
+ *
+ * Return: Pointer to the destination memory area @dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *temp = dest;
+
+	while (n > 0 && *src != '\0')
+	{
+		*temp = *src;
+		src++;
+		temp++;
+		n--;
+	}
+	return (dest);
+}
+
