10e1179a79b1d73ab9bd2c63eb2b6a2c467951d5
Ealise <10503@holbertonstudents.com>
2025-03-05 05:54:58 +0000

    multiple

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -22,10 +22,11 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf("Error");
+		printf("Error\n");
+		return (1);
 	}
 	mul_sum = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", mul_sum);
 
-	return (1);
+	return (0);
 }
