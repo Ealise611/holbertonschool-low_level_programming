d7329d0808b90b9a5605e8d932dde9d1eb30aa4d
Ealise <10503@holbertonstudents.com>
2025-03-10 10:31:42 +0000

    duplicated

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -23,8 +23,12 @@
     }
 
     for (len = 0; str[len] != '\0'; len++)
+    ;
+
+    duplicate = malloc((len + 1) * sizeof(char));
+    if (duplicate == NULL)
     {
-        duplicate = malloc((len + 1) * sizeof(char));
+	return (NULL);
     }
     for (i = 0; i <= len; i++)
     {
