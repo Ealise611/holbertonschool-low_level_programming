49448616ff53ce92d64a637ec2bcdc65ecab9023
Ealise <10503@holbertonstudents.com>
2025-03-10 10:35:05 +0000

    duplicated

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -8,32 +8,30 @@
  * Return: Pointer to the newly allocated duplicated string.
  *         Returns NULL if str is NULL or if memory allocation fails.
  *
- * Note: The returned pointer must be freed by the caller to avoid memory leaks.
+ * Note: The returned pointer must be freed
+ * by the caller to avoid memory leaks.
  */
 
- char *_strdup(char *str)
- {
-    int i;
-    int len;
-    char *duplicate;
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *duplicate;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-
-    for (len = 0; str[len] != '\0'; len++)
-    ;
-
-    duplicate = malloc((len + 1) * sizeof(char));
-    if (duplicate == NULL)
-    {
-	return (NULL);
-    }
-    for (i = 0; i <= len; i++)
-    {
-        duplicate[i] = str[i];
-    }
-    return (duplicate);
-
- }
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	duplicate = malloc((len + 1) * sizeof(char));
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= len; i++)
+	{
+		duplicate[i] = str[i];
+	}
+	return (duplicate);
+}
