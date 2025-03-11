7b294514318e8a90286a576e7b5475a3a3a4abc3
Ealise <10503@holbertonstudents.com>
2025-03-11 00:28:07 +0000

    free grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,10 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * free_grid - free grid
+ * @**grid: pointer to *grid
+ * @height: height
+ * Description: this function free the grid previously created
+ */
+
 void free_grid(int **grid, int height)
 {
 	int i;
-	
+
 	if (grid == NULL)
 	{
 		return;
