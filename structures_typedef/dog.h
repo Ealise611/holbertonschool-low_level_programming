630be2789775b7622e57fd3a75c029f33d39bf3a
Ealise <10503@holbertonstudents.com>
2025-03-12 01:36:31 +0000

    struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,11 +2,12 @@
 #define MAIN_H
 
 /**
- * struct dog 
+ * struct dog - short description
  * @*name: name
  * @*owner: owner
  * @age: owner age
  *
+ * Description: Long description
  */
 
 struct dog
