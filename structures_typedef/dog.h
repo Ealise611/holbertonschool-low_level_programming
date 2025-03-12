edccf13e60c41275225c9442a24b446ccfe747bf
Ealise <10503@holbertonstudents.com>
2025-03-12 01:33:11 +0000

    struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog 
+ * @*name: name
+ * @*owner: owner
+ * @age: owner age
+ *
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
