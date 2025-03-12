5ed4f4a746b51204415bda1f1472c89f64aa829c
Ealise <10503@holbertonstudents.com>
2025-03-12 03:05:19 +0000

    initialise

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -33,37 +33,6 @@ typedef struct dog dog_t;
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-/**
- * print_dog - Prints the attributes of a dog structure
- * @d: Pointer to the dog structure to be printed
- *
- * Description: This function prints the attributes of a dog, including
- * its name, age, and owner's name. If any of the attributes are NULL,
- * it prints (nil) instead.
- */
-void print_dog(struct dog *d);
-
-/**
- * new_dog - Creates a new dog structure
- * @name: Pointer to a string containing the dog's name
- * @age: Age of the dog in years
- * @owner: Pointer to a string containing the owner's name
- *
- * Return: Pointer to the newly allocated dog structure, or NULL on failure
- *
- * Description: This function dynamically allocates memory for a new dog
- * structure and initializes it with the given name, age, and owner.
- */
-dog_t *new_dog(char *name, float age, char *owner);
-
-/**
- * free_dog - Frees memory allocated for a dog structure
- * @d: Pointer to the dog structure to be freed
- *
- * Description: This function frees the memory allocated for a dog
- * structure, including the memory for the name and owner strings.
- */
-void free_dog(dog_t *d);
 
 
 #endif
