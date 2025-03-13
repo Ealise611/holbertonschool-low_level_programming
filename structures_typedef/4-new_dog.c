dc6bceb167f454d94100fff544c0e2913690129f
Ealise <10503@holbertonstudents.com>
2025-03-13 03:27:59 +0000

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,64 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
-/**
- * _strncpy - Copies a string up to a specified number of bytes.
- * @dest: Pointer to the destination buffer where the content is to be copied.
- * @src: Pointer to the source string to be copied.
- * @n: Maximum number of bytes to be copied from @src.
- *
- * Description: This function copies up to @n bytes from the string pointed to
- * by @src to the buffer pointed to by @dest. If length of @src is less than
- * @n, the remainder of @dest will be padded with null bytes ('\0'). If @src
- * contains @n or more bytes, it does not need to be null-terminated, and @dest
- * will not be null-terminated if there's no null byte among the 1st @n bytes
- * of @src. The behavior is undefined if the memory areas of @dest and @src
- * overlap.
- *
- * Return: Pointer to the destination string @dest.
- */
-
-
-char *_strncpy(char *dest, char *src, int n)
-{
-	char *update_dest = dest;
-
-	while (n > 0 && *src != '\0')
-	{
-		*update_dest = *src;
-		src++;
-		update_dest++;
-		n--;
-	}
-	while (n > 0)
-	{
-		*update_dest = '\0';
-		src++;
-		update_dest++;
-		n--;
-	}
-return (dest);
-}
-
-
-/**
- * _strlen - Returns the length of a string.
- * @s: Pointer to the string.
- *
- * Return: The length of the string.
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-	int i;
-
-	for (i = 0; s[len] != '\0'; i++)
-	{
-		len++;
-	}
-	return (len);
-}
 /**
  * new_dog - Creates a new dog structure
  * @name: Pointer to the string containing the dog's name
@@ -76,26 +19,43 @@ int _strlen(char *s)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *s;
+	int len_name = 0, len_owner = 0;
 	dog_t *dog;/*declare the structure pointer to dog_t*/
 
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
+	while (name[len_name] != '\0')
+		len_name++;/*find the length for name*/
+	while (owner[len_owner] != '\0')
+		len_owner++;/*find the length for owner*/
+
+	dog = malloc(sizeof(dog_t));/*allocate memory for structure*/
 	if (dog == NULL)
 	{
+		free(dog);
 		return (NULL);
-		
 	}
-	s = _strlen(len);
-
-	dog = malloc(sizeof(dog_t));
-	if (dog_t == NULL)
+	dog->name = malloc(sizeof(len_name + 1)); /*allocate memory for name*/
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
 		return (NULL);
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-	return (new_dog);
+	}
+	dog->owner = malloc(sizeof(len_owner + 1)); /*allocate memory for owner*/
+	if (dog->owner == NULL)	
+	{
+		free(dog->owner);
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
 }
 
