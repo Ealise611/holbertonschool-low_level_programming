71af976a36d011ef14b237a830126740db6b61d5
Ealise <10503@holbertonstudents.com>
2025-03-15 00:09:20 +0000

    free dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -5,7 +5,7 @@
  * free_dog - Frees the memory allocated for a dog structure and its members
  * @d: Pointer to the dog structure whose memory is to be freed
  *
- * Description: This function frees the memory allocated for the name and owner 
+ * Description: This function frees the memory allocated for the name and owner
  * fields of a dog structure. It also frees the structure itself. It checks 
  * if the fields and the structure are not NULL before freeing them to avoid 
  * undefined behavior.
