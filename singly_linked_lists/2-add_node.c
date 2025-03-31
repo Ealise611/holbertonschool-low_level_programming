d408d4a57f5c2428a75ed5a606cf586d91abcf6c
Ealise <10503@holbertonstudents.com>
2025-03-31 09:35:55 +0000

    add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -25,29 +25,36 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
-        if (new_node == NULL)
-                return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-        new_node->str = strdup(str);
-        if (new_node->str == NULL)
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return(NULL);
+		return (NULL);
 	}
 
 	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
-	return(new_node);
+	return (new_node);
 }
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the string.
+ *
+ * Return: The length of the string.
+ */
 
 int _strlen(const char *s)
 {
 	int len = 0;
+
 	while (s[len] != '\0')
 		len++;
 	return (len);
 }
-
 
