ea970550051d44a2f77e923891b4c9e760876dd2
Ealise <10503@holbertonstudents.com>
2025-03-31 07:17:59 +0000

    num of elements

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,4 +1,32 @@
-#include <stdio.h>
+
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
+/**
+ * list_len - Prints number of elements of a list_t list
+ * @h: Pointer to the head of the linked list
+ *
+ * Description: Iterates through a linked list of type list_t
+ * and return number of elements
+ *
+ * Return: The number of elements in the list
+ */
 
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			return (1);
+		else
+		{
+			h = h->next;
+			count++;
+		}
+	}
+	return (count);
+}
