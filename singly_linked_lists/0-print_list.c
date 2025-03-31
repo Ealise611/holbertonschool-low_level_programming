1f83d24d10d64fdf6478602ee7892dc7743af798
Ealise <10503@holbertonstudents.com>
2025-03-31 07:06:57 +0000

    printlist

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,40 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to the head of the linked list
+ *
+ * Description: Iterates through a linked list of type list_t
+ * and prints each node's string along with its length. If a
+ * string in a node is NULL, it prints [0] (nil) instead.
+ *
+ * Return: The number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}	
+
+
+
