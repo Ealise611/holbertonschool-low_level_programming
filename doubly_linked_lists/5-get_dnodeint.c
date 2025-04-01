5ae3bafc539e770c3d3e81cf683b7589369f8612
Ealise <10503@holbertonstudents.com>
2025-04-01 09:46:29 +0000

    get the nth node

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,13 +4,13 @@
 #include "lists.h"
 
 /**
- * get_dnodeint - get a node at the nth position of a dlistint_t list.
+ * get_dnodeint_at_index - get a node at the nth position of a dlistint_t list.
  * @head: A double pointer to the head of the doubly linked list.
  * @index: The nth node.
  *
  * Description: This function traverses a doubly linked list to
  * find and return the node locateded at the given index.
- * if the node does not exisit, return NULL 
+ * if the node does not exisit, return NULL
  * Index starts from 0, if the node does not existi return NULL.
  *
  * Return: The nth node of a dlistint_t list, or NULL if not exist.
@@ -21,11 +21,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int current_index = 0;
 
 	dlistint_t *temp = head;
-	
+
 	while (temp != NULL)
 	{
 		if (current_index == index)
 			return (temp);
+		
 		else
 		{
 			temp = temp->next;
@@ -33,4 +34,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		}
 	}
 	return (NULL);
-}	
+}
