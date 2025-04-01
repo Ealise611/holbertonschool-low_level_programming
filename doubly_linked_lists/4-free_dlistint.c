5109fdc3c7285856c190d25a4253b9b904a44421
Ealise <10503@holbertonstudents.com>
2025-04-01 05:32:57 +0000

    free

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t linked list
+ * @head: Pointer to the head of the linked list
+ *
+ * Description: Iterates through a linked list of type list_t
+ * and frees each node's string and the node itself, ensuring
+ * all allocated memory is properly deallocated.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	/*declaring a temp pointer*/
+	dlistint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		temp = head
+		head = head->next;
+		free(temp);
+	}
+}
