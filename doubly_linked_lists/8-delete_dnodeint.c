6573cea84cc04bc4feb2936de8224d346517522c
Ealise <10503@holbertonstudents.com>
2025-04-03 05:31:36 +0000

    delete node

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -29,7 +29,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = temp->next;
 		if (*head != NULL)
 		{
-			temp->prev = NULL;
+			(*head)->prev = NULL;
 		}
 		free(temp);
 		return (1);
