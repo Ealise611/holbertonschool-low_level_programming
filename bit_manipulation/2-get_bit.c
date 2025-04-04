60e908f58c8c655e613db2eae9d20ae436f4d1f5
Ealise <10503@holbertonstudents.com>
2025-04-04 05:49:37 +0000

    get bits

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -16,11 +16,7 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index >= total_bits)
 		return (-1);
-	print_binary(n);
 
-	if ((n >> index & 1))
-		return (1);
-	else
-		return (0);
+	return ((n >> index & 1));
 }
 
