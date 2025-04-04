c62b82749e186a435af2aed2a6c06ddd847a7b20
Ealise <10503@holbertonstudents.com>
2025-04-04 05:13:06 +0000

    decimal to binary

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -6,12 +6,12 @@
  * @n: The number to be represented in binary.
  *
  * Description: This function outputs the binary representation of the given
- *              unsigned long integer 'n'. It does so without utilizing arrays,
- *              dynamic memory allocation (malloc), the modulus (%) or division
- *              (/) operators. The function handles edge cases, such as when 'n'
- *              is 0, by printing '0'. For all other values, it determines the
- *              highest bit set and iterates through each bit position, printing
- *              '1' or '0' accordingly.
+ * unsigned long integer 'n'. It does so without utilizing arrays,
+ * dynamic memory allocation (malloc), the modulus (%) or division
+ * (/) operators. The function handles edge cases, such as when 'n'
+ * is 0, by printing '0'. For all other values, it determines the
+ * highest bit set and iterates through each bit position, printing
+ * '1' or '0' accordingly.
  *
  * Constraints:
  *      - Arrays are not used.
@@ -21,14 +21,12 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		_putchar(0);
-	}
-
 	int total_bits = sizeof(n) * 8;
 	int index = total_bits - 1;
 	int started = 0;/*flag to print or not*/
+
+	if (n == 0)
+		_putchar(0);
 
 	while (index >= 0)
 	{
@@ -36,8 +34,7 @@ void print_binary(unsigned long int n)
 		{
 			_putchar('1');
 			started = 1;
-		}
-		
+		}	
 		else if (started)
 			_putchar('0');
 		index--;
