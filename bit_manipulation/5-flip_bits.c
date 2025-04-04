b433bcd56d2533bedb8241b86373f329f7087a6b
Ealise <10503@holbertonstudents.com>
2025-04-04 06:24:11 +0000

    flip bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * flip_bits - Returns the number of bits you would need to flip to get from one number to another.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits needed to flip to convert `n` to `m`.
+ *         This is calculated by comparing the bits of `n` and `m`.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count = 0;
+	int diff;
+	for (diff = n^m; diff != 0; diff = diff>> 1)
+		count += diff & 1;
+	return (count);
+}
