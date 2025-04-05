cb301dcd6e7fc88805e7e72178eca8c1c3c92df8
Ealise <10503@holbertonstudents.com>
2025-04-05 04:27:50 +0000

    create and write

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -40,7 +40,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[s])
 			s++;
-		fdw = write (fd, text_content, s);
+		fdw = write(fd, text_content, s);
 		if (fdw != s)
 			return (-1);
 	}
