a1410246c266cdecf997a943f59596e67e6b7c3a
Ealise <10503@holbertonstudents.com>
2025-04-05 04:27:10 +0000

    create and write

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		open(filename,O_CREAT, 0600);
+		open(filename, O_CREAT, 0600);
 	}
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
