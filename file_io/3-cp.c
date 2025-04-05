5f4b3f8751fe3c148a86c66bd630fb37c20e1476
Ealise <10503@holbertonstudents.com>
2025-04-05 22:22:22 +0000

    copy file from to

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -51,7 +51,11 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1],O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+		if (errno == EACCES)
+			dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+		else
+			dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+
 		exit(98);
 	}
 	/*open file_to for writing, create if needed, truncate if exist*/
