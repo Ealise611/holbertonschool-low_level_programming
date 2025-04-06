c54b1296d7e9151c889fed6ae670dbcfa0f112cb
Ealise <10503@holbertonstudents.com>
2025-04-06 10:01:32 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -57,15 +57,13 @@ int main(int argc, char *argv[])
 	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(file_from);
 		exit(98);
 	}
 	/*open file_to for writing, create if needed, truncate if exist*/
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	/*cp file*/
 	while (r > 0)
