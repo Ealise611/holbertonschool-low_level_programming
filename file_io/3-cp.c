052eab4016dc054fa3c900c2f792a37c5ed8e96b
Ealise <10503@holbertonstudents.com>
2025-04-06 09:48:33 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -39,7 +39,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, w,r;
+	int file_from, file_to, w, r;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
@@ -50,9 +50,16 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	r = read(file_from, buffer, BUFFER_SIZE);
 
-	if (file_from == -1 || r == -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	r = read(file_from, buffer, BUFFER_SIZE);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(file_from);
 		exit(98);
 	}
 	/*open file_to for writing, create if needed, truncate if exist*/
@@ -64,18 +71,26 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 	/*cp file*/
-	while ((r = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	while (r > 0)
 	{
 		w = write(file_to, buffer, r);
-		if (w != r || w == -1)
+		if (w == -1)
 		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 			close(file_from);
 			close(file_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			exit(99);
+		}
+		r = read(file_from, buffer, BUFFER_SIZE);
+		if (r == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close(file_from);
+			close(file_to);
+			exit(98);
 		}
 	}
-	if (r == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
 
 	if (close(file_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
