e82791f2c7b0a5673fedb668f2033a6eb32847b4
Ealise <10503@holbertonstudents.com>
2025-04-06 07:56:20 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 		{
 			close(file_from);
 			close(file_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]), exit(98);
 		}
 	}
 	if (bytes_read == -1)
