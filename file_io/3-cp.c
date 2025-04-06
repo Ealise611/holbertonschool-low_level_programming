ad9ccf2750e4b37df3842c86d0a3dbfb8d104c47
Ealise <10503@holbertonstudents.com>
2025-04-06 00:45:35 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -37,8 +37,7 @@
   */
  int main(int argc, char *argv[])
  {
- 	int file_from, file_to;
- 	ssize_t bytes_read, bytes_written;
+ 	int file_from, file_to, bytes_read, bytes_written;
  	char buffer[BUFFER_SIZE];
  
  	if (argc != 3)
@@ -59,20 +58,17 @@
  		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]), exit(99);
  	}
  	/*cp file*/
-	bytes_read = 1;
  
- 	while (bytes_read)
+ 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
  	{
-		bytes_read = read(file_from, buffer, BUFFER_SIZE);
- 		if (bytes_read == -1)
+		bytes_written = write(file_to, buffer, bytes_read);
+ 		if (bytes_written != bytes_read || bytes_written == -1)
  		{
  			dprintf(STDERR_FILENO,"Error: Can't read to %s\n", argv[2]), exit(99);
  		}
-		if (bytes_read > 0)
+		if (bytes_read == -1)
 		{
-			bytes_written = write(file_to, buffer, bytes_read);
-			if (bytes_written != bytes_read || bytes_written == -1)
-				dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO,"Error: Can't read from %s\n", argv[1]), exit(98);
  		}
 	}
  	if (close(file_from) == -1)
